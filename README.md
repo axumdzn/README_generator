186cb8ba84016fec79454719de6931e2166ba58d
Kenneth Felix <kennf03@gmail.com>
2021-10-14 19:37:04 -0400

    fixed some broken code

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# README_generator
+# README_Generator
+## Description
+ This program is one that creates a read me based on written README file based on the information given to the program.
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+Copy files and run in terminal
+## Usage
+![screenshot](images/terminal.png)
+![readme]()
+## License
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg) (https://opensource.org/licenses/MIT)
+## How to Contribute
+
+## Tests
+
+## Questions
+If you have any questions contact me on
+
+GitHub: (github.com/axumdzn)
+Email: (kenfelixcs@gmail.com)
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const badge;
 const promtUser = () => {
     return inquirer.prompt([
         {
@@ -65,36 +64,45 @@ const generateMarkdown = (data) =>
 ${data.installInstruction}
 ## Usage
 ${data.usageInfo}
-## Credits
 ## License
-${badge}
+${badge} ${link}
 ## How to Contribute
 ${data.contribute}
 ## Tests
 ${data.test}
 ## Questions
 If you have any questions contact me on
-    GitHub: ${data.github}
-    Email: ${data.email}
+GitHub: (github.com/${data.github})
+Email: (${data.email})
 `
-
+let badge;
+let link;
 const init = () => {
     promtUser()
     .then((answers) => {
         switch (answers.license) {
-            case 'MIT': badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            case 'MIT': {badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+            link = "(https://opensource.org/licenses/MIT)";
+        }
                 break;
-            case 'Apache': badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            case 'Apache': {badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+            link = "(https://opensource.org/licenses/Apache-2.0)";
+        }
                 break;
-            case 'Boost': badge = '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+            case 'Boost': {badge = '![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)';
+            link = "(https://www.boost.org/LICENSE_1_0.txt)"
+        }
                 break;
-            case 'BSD': badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+            case 'BSD': {badge = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
+            link = "(https://opensource.org/licenses/BSD-3-Clause)";
+        }
                 break;   
-            default: badge = '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
+            default: {badge = '![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)'
+            link = "(https://opensource.org/licenses/BSD-3-Clause)";
+        }
                 break;
         }
-    })
-    .then((answers) => fs.writeFileSync('testREADME.md', generateMarkdown(answers)))
+        fs.writeFileSync('README.md', generateMarkdown(answers))})
     .then(() => console.log('Great Sucess'))
     .catch((error) => console.error(error))
 }
